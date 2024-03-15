bfe1a3e58c0bbe909e23de9c86aa9cef663487c4
Rafael Maia de Souza <rmaiadesouza@gmail.com>
2024-03-14 22:45:54 -0300

    Incluindo função para barra de pesquisa e botões de categoria.

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ async function buscarEMostrarVideos(){
                     <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
                     <h3 class="titulo-video">${video.titulo}</h3>
                     <p class="titulo-canal">${video.descricao}</p>
+                    <p class="categoria" hidden>${video.categoria}</p>
                 </div>
             </li>
             `;
@@ -31,3 +32,43 @@ async function buscarEMostrarVideos(){
 }
 
 buscarEMostrarVideos();
+
+const barraDePesquisa = document.querySelector(".pesquisar__input");
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa() {
+    const videos = document.querySelectorAll('.videos__item');
+    const valorFiltro = barraDePesquisa.value.toLowerCase();
+  
+    videos.forEach((video) => {
+      const titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+  
+      video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? 'block' : 'none' : 'block';
+    });
+  }
+
+  const botaoCategoria = document.querySelectorAll('.superior__item');
+
+  botaoCategoria.forEach((botao) => {
+    let nomeCategoria = botao.getAttribute("name");
+
+    botao.addEventListener('click', ()=> filtrarPorCategoria(nomeCategoria))
+
+  })
+
+  function filtrarPorCategoria(filtro){
+    const videos = document.querySelectorAll('.videos__item');
+
+    videos.forEach((video) => {
+        let categoria = video.querySelector('.categoria').textContent.toLocaleLowerCase();
+        let valorFiltro = filtro.toLocaleLowerCase();
+
+        if(!categoria.includes(valorFiltro) && valorFiltro != 'tudo')  {
+            video.style.display = 'none';
+        } else {
+            video.style.display = 'block';
+        }
+        
+    })
+  }
